a5fea726dc8f997202831732c69ae4c2dd07050c
tyshenko.roma <tyshenko.roma@gmail.com>
2020-07-16 18:11:01 +0300

    Added all tests exept the last one

---
+++ src/test/java/folder/ParserSteps.java
@@ -0,0 +1,83 @@
+package folder;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParserSteps {
+
+    private Response response;
+    private String id;
+    private int cardNumber;
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "https://deckofcardsapi.com/api/deck";
+    }
+
+    @Given("I have new game with {string} decks")
+    public void iHaveNewDescWithDecks(String number) {
+        response = RestAssured.given().when().get("/new/shuffle/?deck_count=" + number);
+        id = response.body().jsonPath().getString("deck_id");
+    }
+
+    @When("I drow {string} cards")
+    public void iDrowCards(String times) {
+        for (int i = 0; i < Integer.parseInt(times); i++) {
+            response = RestAssured.given().when().get("/" + id + "/draw/?count=2");
+        }
+        cardNumber = response.body().jsonPath().getInt("remaining");
+    }
+
+    @Then("card count in deck is changed to {string}")
+    public void cardCountInDeckIsChangedTo(String arg0) {
+        int expectedCount = Integer.parseInt(arg0);
+
+        Assert.assertEquals(cardNumber, expectedCount);
+    }
+
+    @Given("User has deck with Aceses only")
+    public void userHaveDeckDescWithAcesesOnly() {
+        response = RestAssured.given().when().get("/new/shuffle/?cards=AS,AD,AC,AH");
+        id = response.body().jsonPath().getString("deck_id");
+    }
+
+    @Then("User can get Aceses only")
+    public void userCanGetAcesesOnly() {
+        response = RestAssured.given().when().get(id + "/draw/?count=4");
+        final List<String> acesesList = response.body().jsonPath().getList("cards.code");
+
+        Assert.assertTrue(acesesList.stream().allMatch(s -> s.contains("A")));
+    }
+
+    @Given("User has new game with one deck")
+    public void userHasNewGameWithOneDeck() {
+        response = RestAssured.given().when().get("/new/shuffle/?deck_count=1");
+        id = response.body().jsonPath().getString("deck_id");
+    }
+
+    @When("User gets five cards")
+    public void userGetsFiveCards() {
+        response = RestAssured.given().when().get("/" + id + "/draw/?count=5");
+    }
+
+    @Then("card amound was changed")
+    public void cardAmoundWasChanged() {
+        int actualAmound = response.body().jsonPath().getInt("remaining");
+        int expectedAmound = 47;
+
+        Assert.assertEquals(expectedAmound, actualAmound);
+    }
+
+    @Then("cards not repeat in the desk after drawning")
+    public void cardsNotRepeatInTheDeskAfterDrawning() {
+
+    }
+}
--- src/test/resources/descOfCards.feature
+++ src/test/resources/descOfCards.feature
@@ -1,9 +1,26 @@
 Feature: Cucumber run test
 
-  I want to run a sample feature file.
+  I want to play cards
 
-  Scenario: Cucumber setup
+  Scenario Outline: Check if card count in deck is correct after drawing X cards from it
 
-    Given sample feature file is ready
-    When I run the feature file
-    Then run should be successful
+    Given I have new game with "6" decks
+    When I drow "<times>" cards
+    Then card count in deck is changed to "<remaining>"
+    Examples:
+      | times | remaining |
+      | 1     | 310       |
+      | 2     | 308       |
+      | 10    | 292       |
+
+  Scenario:  Check if user can get only Aceses from desc with Aceses only
+
+    Given User has deck with Aceses only
+    Then User can get Aceses only
+
+  Scenario:  Check that cards not repeat in the desk after drawning
+
+    Given User has new game with one deck
+    When User gets five cards
+    Then card amound was changed
+    Then cards not repeat in the desk after drawning
